bebdaf9f8d3b6840e413bc96f5d488eb5a56d424
shivamsinghcse <shivamsinghcse21@gmail.com>
2023-10-23 19:37:23 +0530

    changes

--- .next/types/app/adminpanelsignup/page.ts
+++ .next/types/app/adminpanelsignup/page.ts
@@ -1,4 +1,4 @@
-// File: /home/shivam/Projects/WebPlenty/app/adminpanelsignup/page.tsx
+// File: /home/shivam/WebPlenty/app/adminpanelsignup/page.tsx
 import * as entry from '../../../../app/adminpanelsignup/page.js'
 import type { ResolvingMetadata } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
