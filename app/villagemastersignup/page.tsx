80cf930a17bd474fb39fb4a612ed58989b16e798
shivamsinghcse <shivamsinghcse21@gmail.com>
2023-10-24 23:59:01 +0530

    village

--- app/villagemastersignup/page.tsx
+++ app/villagemastersignup/page.tsx
@@ -2,15 +2,17 @@
 import { NavbarLogout } from "../components/navbarlogout";
 import { Sidebar } from "../components/sidebar";
 import { useForm, Controller } from "react-hook-form";
-
 import { api, baseURL } from "../pages/api";
 import useSWR from "swr";
 import toast, { Toaster } from "react-hot-toast";
 import { SendToBack } from "lucide-react";
+import { useState } from "react";
+
 function Page() {
   const {
     control,
     handleSubmit,
+    register,
     formState: { errors },
   } = useForm();
 
@@ -27,38 +29,39 @@ function Page() {
             background: "#333",
             color: "#fff",
           },
-          
         });
         setTimeout(() => {
           window.location.reload();
         }, 500);
-        
       })
       .catch(function (error) {
         toast.error(error.response.data.message);
       });
-      function getAuthToken() {
-        return localStorage.getItem('accessToken');
-      }
-      const fetcher = (url: string) =>
-      fetch(url, {
-        headers: {
-          authorization: `${getAuthToken()}`, // Include the token here
-        },
-      }).then((res) => res.json());
-      const { data, error, isLoading } = useSWR(`${baseURL}/sector`, fetcher);
-  };
+  }
+
+  function getAuthToken() {
+    return localStorage.getItem('accessToken');
+  }
+
+  const fetcher = (url: string) => fetch(url, {
+    headers: {
+      authorization: `${getAuthToken()}`,
+    },
+  }).then((res) => res.json());
+
+  const { data, error, isLoading } = useSWR(`${baseURL}/sector`, fetcher);
+
   return (
     <>
-      <div className="w-[100vw]  z-10">
+      <div className="w-[100vw] z-10">
         <NavbarLogout />
       </div>
       <div className="flex">
         <div>
           <Sidebar />
         </div>
-        <Toaster/>
-        <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+        <Toaster />
+        <div className="absolute top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2">
           <form className="w-full max-w-lg" onSubmit={handleSubmit(onSubmit)}>
             <h1 className="text-2xl">Village Master Form</h1>
             <div className="flex flex-wrap -mx-3 mb-6 mt-5">
@@ -79,7 +82,7 @@ function Page() {
                     />
                   )}
                 />
-                {errors.villageName && (
+                {errors.name && (
                   <p className="text-red-500 text-xs italic">
                     This field is required
                   </p>
@@ -88,35 +91,34 @@ function Page() {
                   Sector
                 </label>
                 <select
-  {...register("sectorId", { required: true })}
-  className="w-full p-2 mb-4 border rounded-md"
->
-  {!isLoading ? (
-    data && data.data ? (
-      data.data.map((info: any) => (
-        <option value={info.id} key={info.id}>
-          {info.name}
-        </option>
-      ))
-    ) : (
-      <option value="" key="no-data">
-        No data available
-      </option>
-    )
-  ) : (
-    <option value="" key="loading">
-      Loading...
-    </option>
-  )}
-</select>
-
+                  {...register("sectorId", { required: true })}
+                  className="w-full p-2 mb-4 border rounded-md"
+                >
+                  {!isLoading ? (
+                    data && data.data ? (
+                      data.data.map((info: any) => (
+                        <option value={info.id} key={info.id}>
+                          {info.name}
+                        </option>
+                      ))
+                    ) : (
+                      <option value="" key="no-data">
+                        No data available
+                      </option>
+                    )
+                  ) : (
+                    <option value="" key="loading">
+                      Loading...
+                    </option>
+                  )}
+                </select>
               </div>
-              
             </div>
 
             <button
               type="submit"
-              className="appearance-none block w-full bg-orange-600 text-white border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              className="w-full bg-orange-600 text-white border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              {...handleSubmit}
             >
               Submit
             </button>
@@ -126,4 +128,5 @@ function Page() {
     </>
   );
 }
+
 export default Page;
