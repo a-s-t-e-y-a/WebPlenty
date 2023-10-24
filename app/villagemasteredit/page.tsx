f31a7c70aa24ad1fd4b940fdb28c819cf03e2b8e
a-s-t-e-y-a <krishnoit100@gmail.com>
2023-10-25 01:25:47 +0530

    ✨ New feature new

--- app/villagemasteredit/page.tsx
+++ app/villagemasteredit/page.tsx
@@ -1,97 +1,82 @@
-'use client'
+"use client";
 import { NavbarLogout } from "../components/navbarlogout";
 import { Sidebar } from "../components/sidebar";
-import { useForm, Controller, SubmitHandler } from "react-hook-form";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
 import { api } from "../pages/api";
 import toast, { Toaster } from "react-hot-toast";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+
 export default function Page() {
-    interface villageFormData {
-        name: string;
-    
-        
-        sectorId?: number | null;
-      }
-      type FormValue = {
-        name: string
-        // address: string;
-        // mobileNumber: string;
-        // dob: string;
-        // religion: string;
-        // gender: string;
-        // previousParty?: string;
-        SectorId: number;
-        role: string;
-      };
-      
-      async function Postvillage(data: villageFormData) {
+  const [info, setInfo] = useState(null); // Specify the type for `info` or initialize it as null
+  const [load, setLoad] = useState(true);
+  const searchParams = useSearchParams();
+  const router = useRouter()
+  useEffect(() => {
+    const dataParam = searchParams.get("data");
+    if (dataParam) {
+      console.log(JSON.parse(dataParam));
+      setInfo(JSON.parse(dataParam));
+      setLoad(false);
+    }
+  }, [searchParams]);
+
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm(); // You need to specify the type for `FormValue`.
+
+  const onSubmit: SubmitHandler<FormValue> = (data) => {
         return api
-          .post("/village", {
-            ...data,
-          })
-          .then(function (response) {})
-          .catch(function (error) {
-            toast.error(error.response.data.message);
-      
-            console.log(error.response.data.message);
-          });
-      }
-      
-      const villageForm: React.FC<villageFormProps> = (props) => {
-        const [info, setInfo] = useState<villageData>();
-        const [load, setLoad] = useState(true);
-        const searchParams = useSearchParams();
-        useEffect(() => {
-          const dataParam = searchParams.get("data");
-          if (dataParam) {
-            console.log(dataParam);
-            setInfo(JSON.parse(dataParam));
-            setLoad(false);
-          }
-        }, []);
-      
-        const {
-          register,
-          handleSubmit,
-          formState: { errors },
-        } = useForm<FormValue>();
-        const onSubmit: SubmitHandler<FormValue> = (data: any) => {
-          postvillage(data);
-        };
-        async function postvillage(data: villageFormData) {
-          console.log(data);
-          return api
-            .put(`/village/${info ? info.id : ""}`, {
-              ...data,
-            })
-            .then(function (response) {
-              toast(response.data.message, {
-                icon: "👏",
-                style: {
-                  borderRadius: "10px",
-                  background: "#333",
-                  color: "#fff",
-                },
-              });
-            })
-            .catch(function (error) {
-              toast.error(error.response.data.message);
-            });
-        }
-        if (load) return <div> Load...</div>;
-      
+      .put(`/village/${info ? info.id : ""}`, {
+        ...data,
+        sectorId:info.sector.id
+      })
+      .then(function (response) {
+        router.push('/villagemaster')
+        toast(response.data.message, {
+          icon: "👏",
+          style: {
+            borderRadius: "10px",
+            background: "#333",
+            color: "#fff",
+          },
+        });
+      })
+      .catch(function (error) {
+        toast.error(error.response.data.message);
+      });
+
+  };
+
+  async function Postvillage(data) {
+    try {
+      const response = await api.post("/village", data);
+      // Handle the response if necessary
+    } catch (error) {
+      toast.error(error.response.data.message);
+      console.log(error.response.data.message);
+    }
+  }
+
+  if (load) return <div>Load...</div>;
+
   return (
     <>
-      <div className="w-[100vw]  z-10">
+      <div className="w-[100vw] z-10">
         <NavbarLogout />
       </div>
       <div className="flex">
         <div>
           <Sidebar />
         </div>
-        <Toaster/>
+        <Toaster />
         <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
           <form className="w-full max-w-lg" onSubmit={handleSubmit(onSubmit)}>
-            <h1 className="text-2xl">Village Master Form</h1>
+            <h1 className="text-2xl">Village Master Edit Form</h1>
             <div className="flex flex-wrap -mx-3 mb-6 mt-5">
               <div className="w-full px-3">
                 <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
@@ -100,7 +85,7 @@ export default function Page() {
                 <Controller
                   name="name"
                   control={control}
-                  defaultValue="name"
+                  defaultValue={info.name}
                   render={({ field }) => (
                     <input
                       {...field}
@@ -110,40 +95,23 @@ export default function Page() {
                     />
                   )}
                 />
-                {errors.villageName && (
+                {errors.name && (
                   <p className="text-red-500 text-xs italic">
                     This field is required
                   </p>
                 )}
-                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                  Sector
-                </label>
-                <Controller
-                  name="sector"
-                  control={control}
-                  defaultValue=""
-                  render={({ field }) => (
-                    <input
-                      {...field}
-                      className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                      type="text"
-                      placeholder="Sector"
-                    />
-                  )}
-                />
               </div>
-              
             </div>
 
             <button
               type="submit"
               className="appearance-none block w-full bg-orange-600 text-white border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
             >
-              Submit
+              Update
             </button>
           </form>
         </div>
       </div>
     </>
   );
-}}
+}
