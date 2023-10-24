52ad6db9c21650891501ed26c4aa284ee197ce2d
shivamsinghcse <shivamsinghcse21@gmail.com>
2023-10-25 02:18:27 +0530

    fixing build

---
+++ app/sectormasterdetails/page.tsx
@@ -0,0 +1,107 @@
+"use client";
+import Link from "next/link";
+import { Navbar } from "../components/navbar";
+import { Sidebar } from "../components/sidebar";
+import toast, { Toaster } from "react-hot-toast";
+import { NavbarLogout } from "../components/navbarlogout";
+import { useState, useEffect } from "react";
+import api from "../pages/api";
+
+export default function Page() {
+  const [data, setData] = useState([]); // Replace with your actual data
+
+  useEffect(() => {
+    // Fetch your data here
+    const fetchData = async () => {
+      try {
+        const response = await api.get("/your-api-endpoint"); // Replace with your API endpoint
+        setData(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <div className="w-[100vw] z-10">
+        <NavbarLogout />
+      </div>
+      <div className="flex">
+        <div>
+          <Sidebar />
+        </div>
+
+        <div className="w-full">
+          <div className="flex justify-center">
+            <h1 className="text-2xl font-extrabold mt-10">Sector Name</h1>
+          </div>
+
+          <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                  <th scope="col" className="px-6 py-3">
+                    Sr.No.
+                  </th>
+
+                  <th scope="col" className="px-6 py-3">
+                    Name
+                  </th>
+
+                  <th scope="col" className="px-6 py-3">
+                    Role
+                  </th>
+
+                  <th scope="col" className="px-6 py-3">
+                    Action
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Delete
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* {data.map((info, index) => ( */}
+                <tr
+                  // key={info.id}
+                  className="bg-white border-b dark:bg-gray-900 dark:border-gray-700"
+                >
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    {/* {index + 1} */}
+                  </th>
+                  <td className="px-6 py-4"></td>
+                  <td className="px-6 py-4"></td>
+                  <td className="px-6 py-4">
+                    <Link
+                      href="../mundalmasterformedit"
+                      className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                    >
+                      Update
+                    </Link>
+                  </td>
+                  <td className="px-6 py-4">
+                    <button
+                      // onClick={() => del(info.id)}
+                      className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+                {/* ))} */}
+              </tbody>
+            </table>
+          </div>
+
+          <hr className="my-10" />
+        </div>
+      </div>
+    </>
+  );
+}
