809aa3fce373d92a000cda5210df080864e84aeb
shivamsinghcse <shivamsinghcse21@gmail.com>
2023-10-25 15:30:51 +0530

    ✨ New feature new

--- app/sectormasterformedit/page.tsx
+++ app/sectormasterformedit/page.tsx
@@ -64,7 +64,7 @@ function Page() {
             <div className="flex flex-wrap -mx-3 mb-6 mt-5">
               <div className="w-full px-3">
                 <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                  Mundal Name
+                 Name
                 </label>
                 <Controller
                   name="name"
@@ -75,7 +75,7 @@ function Page() {
                       {...field}
                       className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                       type="text"
-                      placeholder="Jane"
+                      placeholder="Name"
                     />
                   )}
                 />
