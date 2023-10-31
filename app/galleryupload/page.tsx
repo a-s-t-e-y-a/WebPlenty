1c003aed457bf1ccc4444fde451ae904babbfc58
a-s-t-e-y-a <krishnoit100@gmail.com>
2023-10-31 10:53:50 +0530

    🔥 Remove new

--- app/galleryupload/page.tsx
+++ app/galleryupload/page.tsx
@@ -6,49 +6,52 @@ import api from "../pages/api";
 
 export default function Page() {
   const { register, handleSubmit } = useForm();
-  const [selectedImage, setSelectedImage] = useState(null); // State for selected image
+  const [imagePreview, setImagePreview] = useState<string | null>(null);
 
-  const handleFileChange = (event:any) => {
-    const file = event.target.files[0];
-    if (file) {
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedImage = event.target.files;
+
+    if (selectedImage && selectedImage.length > 0) {
       const reader = new FileReader();
-      reader.onload = (e:any) => {
-        setSelectedImage(e.target.result);
+      reader.onload = (e: any) => {
+        setImagePreview(e.target.result as string);
       };
-      reader.readAsDataURL(file);
+      reader.readAsDataURL(selectedImage[0]);
+    } else {
+      setImagePreview(null);
     }
   };
 
-  const onSubmit = (data:any) => {
+  const onSubmit = (data: any) => {
     // Handle form submission here
     console.log(data);
     const formData = new FormData();
     if (data.image) {
       formData.append("image", data.image[0]);
     }
-    return api
-      .post("/gallery", formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      })
-      .then(function (response) {
-        toast.success(response.data.message, {
-          icon: "👏",
-          style: {
-            borderRadius: "10px",
-            background: "#333",
-            color: "#fff",
-          },
-        });
-        setTimeout(() => {
-          window.location.reload();
-        }, 1000);
-      })
-      .catch(function (error) {
-        console.log(error);
-        toast.error(error.response.data.message);
-      });
+    // return api
+    //   .post("/gallery", formData, {
+    //     headers: {
+    //       "Content-Type": "multipart/form-data",
+    //     },
+    //   })
+    //   .then(function (response) {
+    //     toast.success(response.data.message, {
+    //       icon: "👏",
+    //       style: {
+    //         borderRadius: "10px",
+    //         background: "#333",
+    //         color: "#fff",
+    //       },
+    //     });
+    //     setTimeout(() => {
+    //       window.location.reload();
+    //     }, 1000);
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    //     toast.error(error.response.data.message);
+    //   });
   };
 
   return (
@@ -56,14 +59,12 @@ export default function Page() {
       <div className="flex justify-center w-full mx-auto sm:max-w-lg">
         <div className="flex flex-col items-center justify-center w-full h-auto my-20 bg-white sm:w-3/4 sm:rounded-lg sm:shadow-xl">
           <div className="mt-10 mb-10 text-center">
-            {selectedImage && (
-              <div className="mt-4">
-                <img
-                  src={selectedImage}
-                  alt="Selected"
-                  className="w-full h-full rounded-lg"
-                />
-              </div>
+            {imagePreview && (
+              <img
+                src={imagePreview}
+                alt="Image Preview"
+                className="max-h-40 mx-auto mt-4 rounded-md"
+              />
             )}
 
             <h2 className="text-2xl font-semibold mb-2">Upload your files</h2>
@@ -77,29 +78,29 @@ export default function Page() {
           >
             <input
               type="file"
-              id="file-upload"
-              {...register("file")}
-              className="hidden"
-              onChange={handleFileChange}
+              multiple
+              className="cursor-pointer relative block opacity-0 w-full h-full p-20 z-50 rounded-md"
+              {...register("image")}
+              onChange={handleImageChange}
             />
             <div className="absolute w-full h-full border-solid border-indigo-400  border-2">
-            <label
-              htmlFor="file-upload"
-              className="z-20 flex flex-col-reverse items-center justify-center w-full h-full cursor-pointer"
-            >
-              <p className="z-10 text-xs font-light text-center text-gray-500">
-                Drag & Drop your files here
-              </p>
-              <svg
-                className="z-10 w-8 h-8 text-indigo-400"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
+              <label
+                htmlFor="file-upload"
+                className="z-20 flex flex-col-reverse items-center justify-center w-full h-full cursor-pointer"
               >
-                <path d="M2 6a2 2 0 012-2h5l2 2h5a2 2 0 012 2v6a2 2 0 01-2 2H4a2 2 0 01-2-2V6z">
-                </path>
-              </svg>
-            </label>
+                <p className="z-10 text-xs font-light text-center text-gray-500">
+                  Drag & Drop your files here
+                </p>
+                <svg
+                  className="z-10 w-8 h-8 text-indigo-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M2 6a2 2 0 012-2h5l2 2h5a2 2 0 012 2v6a2 2 0 01-2 2H4a2 2 0 01-2-2V6z">
+                  </path>
+                </svg>
+              </label>
             </div>
             <button
               type="submit"
