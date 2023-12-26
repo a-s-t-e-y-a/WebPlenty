a74d28630c05f72a5ddee6543f82a5a25672e33e
shivamsinghcse <shivamsinghcse21@gmail.com>
2023-12-27 00:22:21 +0530

    fixing url of s3

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -8,24 +8,22 @@ import { useRouter, useSearchParams } from 'next/navigation';
 import toast from 'react-hot-toast';
 
 function Page() {
-  const [info, setInfo] = useState<any>(null); // Set the initial state to null
+  const [info, setInfo] = useState<any>(null);
   const [load, setLoad] = useState(true);
   const router = useRouter();
   const searchParams = useSearchParams();
-  
+
   useEffect(() => {
     const dataParam = searchParams.get("data");
     if (dataParam) {
-      console.log(JSON.parse(dataParam));
       setInfo(JSON.parse(dataParam));
       setLoad(false);
     }
   }, []);
 
   const handleFormSubmit = async (data: any) => {
-    console.log(data);
     try {
-      const response = await api.put(`/user/${info?.id}`, data); // Use optional chaining here
+      const response = await api.put(`/user/${info?.id}`, data);
       toast.success(response.data.message, {
         icon: "😎",
         style: {
@@ -39,7 +37,7 @@ function Page() {
       }, 500);
     } catch (error: any) {
       console.log(error);
-      toast.error(error.response?.data?.message || "An error occurred", { // Use optional chaining here and provide a fallback message
+      toast.error(error.response?.data?.message || "An error occurred", {
         icon: "😥",
         style: {
           borderRadius: "10px",
@@ -57,7 +55,12 @@ function Page() {
         <div className="mx-auto sm:w-full md:max-w-[90%] lg:max-w-[80%]">
           <div className="rounded-lg bg-gray-200 p-4">
             <Image
-              src={info?.image || Heroimg} // Use optional chaining and provide a default image (Heroimg) when info is not available
+              src={info?.image.startsWith("https://webplenty.s3.ap-south-1.amazonaws.com/")
+                ? info?.image
+                : info?.image.replace(
+                    "https://shivam-practics-bucket.s3.ap-south-1.amazonaws.com/",
+                    "https://webplenty.s3.ap-south-1.amazonaws.com/"
+                  )}
               alt="Go Back"
               width={1200}
               height={800}
@@ -66,14 +69,10 @@ function Page() {
           </div>
         </div>
       </div>
-      <div className='mx-auto sm:w-full md:max-w-[90%] lg:max-w-[80%]'>
-        <div className='mx-auto p-4'>
-          <h1 className='text-4xl font-bold'>
-            {info ? info.title : "use Client"} {/* Display 'use Client' when info is not available */}
-          </h1>
-          <p className='py-4'>
-            {info ? info.content : ""}
-          </p>
+      <div className="mx-auto sm:w-full md:max-w-[90%] lg:max-w-[80%]">
+        <div className="mx-auto p-4">
+          <h1 className="text-4xl font-bold">{info ? info.title : "use Client"}</h1>
+          <p className="py-4">{info ? info.content : ""}</p>
         </div>
       </div>
     </div>
